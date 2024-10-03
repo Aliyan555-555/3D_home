5ccd5805458dee99463509f4c93bd2bd943f94d5
Aliyan Siddiqui <141032178+Aliyan555-555@users.noreply.github.com>
2024-10-03 04:30:26 +0000

    update

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: D:\3d_home\app\layout.tsx
+// File: /workspaces/panorama-view/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: D:\3d_home\app\page.tsx
+// File: /workspaces/panorama-view/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
